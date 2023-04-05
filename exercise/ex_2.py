d38faa8156871daf25b6f7a29bd4d2233ef9bc3c
Natalia Kuś <66189@ue.poznan.pl>
2023-04-05 19:33:02 +0200

    Dodany prostokąt

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -9,3 +9,13 @@ obwod = a + b + c
 pole = int((h * a) / 2)
 
 print("Obwod trojkata wynosi " + str(obwod) + ", zas pole wynisi " + str(pole) + ".")
+
+#prostokąt
+
+a=10
+b=15
+
+obwod = 2*a+2*b
+pole = a*b
+print("Obwod prostokąta wynosi " + str(obwod) + ", zas pole wynisi " + str(pole) + ".")
+
