bcf89cec212acf97ac16e4ec0826ce390d30d042
Natalia Kuś <66189@ue.poznan.pl>
2023-04-12 21:36:11 +0200

    zadanie 1.10

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -1,11 +1,24 @@
-#zadanie 1.9
-studenci = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
+#zadanie 1.10
+wykres_1 = [[2, 4], [4, 4], [6, 4]]
+wykres_2 = [[2, 3], [4, 4], [6, 5]]
+wykres_3 = [[2, 3], [4, 3], [5, 4]]
 
-liczba_n = 0
-for student in studenci:
-    nazwisko = student.split()[-1]
-    if nazwisko.startswith('N'):
-        liczba_n += 1
+wykres_1_funkcja_liniowa = (wykres_1[1][1] - wykres_1[0][1]) == (wykres_1[2][1] - wykres_1[1][1])
+wykres_2_funkcja_liniowa = (wykres_2[1][1] - wykres_2[0][1]) == (wykres_2[2][1] - wykres_2[1][1])
+wykres_3_funkcja_liniowa = (wykres_3[1][1] - wykres_3[0][1]) == (wykres_3[2][1] - wykres_3[1][1])
 
-print("Liczba studentow na N wynosi:", liczba_n)
+if wykres_1_funkcja_liniowa:
+    print("Dla punktow w wykres_1 mozna wyznaczyc funkcje liniowa.")
+else:
+    print("Dla punktow w wykres_1 nie mozna wyznaczyc funkcji liniowej.")
+
+if wykres_2_funkcja_liniowa:
+    print("Dla punktow w wykres_2 mozna wyznaczyc funkcje liniowa.")
+else:
+    print("Dla punktow w wykres_2 nie mozna wyznaczyc funkcji liniowej.")
+
+if wykres_3_funkcja_liniowa:
+    print("Dla punktow w wykres_3 mozna wyznaczyc funkcje liniowa.")
+else:
+    print("Dla punktow w wykres_3 nie mozna wyznaczyc funkcji liniowej.")
 
