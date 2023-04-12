61c27328856213c107307f966323121ffc2ba007
Natalia Kuś <66189@ue.poznan.pl>
2023-04-12 21:52:31 +0200

    zadanie 1.8

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -47,3 +47,14 @@ studenci.sort(key=lambda x: x.split()[0])
 print("Alfabetyczna lista studentow wynosi:")
 for student in studenci:
     print(student)
+#zadanie 1.8
+studenci = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
+
+
+nazwiska = [student.split()[-1] for student in studenci]
+studenci_sorted = [s for _, s in sorted(zip(nazwiska, studenci))]
+
+
+print("Alfabetyczna lista studentów wynosi:")
+for student in studenci_sorted:
+    print(student)
