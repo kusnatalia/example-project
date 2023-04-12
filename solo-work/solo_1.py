206e44bea4176c65bd4dabdbee8e8a468109887b
Natalia Kuś <66189@ue.poznan.pl>
2023-04-12 21:53:03 +0200

    zadanie 1.9

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -58,3 +58,13 @@ studenci_sorted = [s for _, s in sorted(zip(nazwiska, studenci))]
 print("Alfabetyczna lista studentów wynosi:")
 for student in studenci_sorted:
     print(student)
+#zadanie 1.9
+studenci = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
+
+liczba_n = 0
+for student in studenci:
+    nazwisko = student.split()[-1]
+    if nazwisko.startswith('N'):
+        liczba_n += 1
+
+print("Liczba studentow na N wynosi:", liczba_n)
