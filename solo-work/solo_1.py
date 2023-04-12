d36dceb70913bc536c0878017b0ff7d63ab5c678
Natalia Kuś <66189@ue.poznan.pl>
2023-04-12 20:58:51 +0200

    zadanie 1.5

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -1,27 +1,8 @@
-# zadanie 1.1
-komunikat = "{} {}"
-hello = "Hello"
-student = "Natalia"
+#zadanie 1.5
+liczba = 3
+potega = 4
 
-pelny_komunikat = komunikat.format(hello, student)
-print("Hello Natalia")
+wynik = liczba ** potega
 
-# zadanie 1.2
-
-student = input("Natalia")
-
-print("Hello Natalia")
-
-# zadanie 1.3
-studenci = ["Ania", "Kuba", "Piotr", "Jan"]
-
-liczba_studentow = len(studenci)
-print("Liczba studentow wynosi:", liczba_studentow)
-
-#zadanie 1.4
-studenci = ["Ania", "Kasia", "Piotr", "Tomek"]
-
-# za pomoca petli i print przywitaj sie z kazdym studentem
-for student in studenci:
-    print("Hello", student)
+print("Wynik wynosi:", wynik)
 
